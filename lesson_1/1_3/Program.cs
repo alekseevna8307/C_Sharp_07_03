858418fd6d772c475cea50644fe1628be0b56e15
Anastasiya <vybornova.aa95@gmail.com>
2023-03-14 01:49:53 +0300

    Task 1_3

---
+++ lesson_1/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+int count = -num;
+
+while (count != num)
+{
+  Console.WriteLine(count);
+  if (num < 0)
+  {
+    count--;
+  }
+  else 
+  {
+    count++;
+  }
+}
