038cbf9561efe6196be058a04ac03f1fee160ae4
Anastasiya <vybornova.aa95@gmail.com>
2023-03-14 00:42:19 +0300

    Task 1_1

---
+++ lesson_1/1_1/Program.cs
@@ -0,0 +1,17 @@
+﻿int a = 0;
+int b = 0;
+
+Console.Write("Введите значение a = ");
+a = int.Parse(Console.ReadLine());
+
+Console.Write("Введите значение b = ");
+b = int.Parse(Console.ReadLine());
+
+if((b * b) == a)
+{
+  Console.WriteLine("да");
+}
+else
+{
+  Console.WriteLine("нет");
+}
