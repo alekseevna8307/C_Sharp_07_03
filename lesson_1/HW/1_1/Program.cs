4f26c440ceb24ec74ae59c6cf0bf5579bb6648da
Anastasiya <vybornova.aa95@gmail.com>
2023-03-14 18:32:39 +0300

    HW 1_1

---
+++ lesson_1/HW/1_1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Введите число: ");
+int num1 = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число: ");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+{
+    Console.WriteLine(num1);
+}
+else if (num2 > num1)
+{
+  Console.WriteLine(num2);
+}
+else
+{
+  Console.WriteLine("Введены одинаковые числа");
+}
