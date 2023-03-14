2b66aae4c16c24d24cc1b7e8a6967d91779fbb01
Anastasiya <vybornova.aa95@gmail.com>
2023-03-14 19:00:47 +0300

    HW 1_2

---
+++ lesson_1/HW/1_2/Program.cs
@@ -0,0 +1,27 @@
+﻿Console.Write("Введите число: ");
+int a = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число: ");
+int b = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите число: ");
+int c = int.Parse(Console.ReadLine()!);
+
+int max = a;
+
+if (a > max) 
+max = a;
+
+else if (b > max) 
+max = b;
+
+else if (c > max) 
+max = c;
+
+else 
+{
+  Console.Write("Введены одинаковые числа ");
+}
+  
+Console.Write("max = ");
+Console.WriteLine(max);
