82ec86222fc7c20198798d2171f930242a01ec57
Anastasiya <vybornova.aa95@gmail.com>
2023-03-15 23:07:32 +0300

    HW 1_1

---
+++ lesson_2/HW/1_1/Program.cs
@@ -0,0 +1,9 @@
+﻿int TakeNum(int num)
+{
+  Console.WriteLine(num);
+  int n = num / 10;
+  return n % 10;
+}
+int sec_num = new Random().Next(100, 1000);
+int result = TakeNum(sec_num);
+Console.WriteLine(result);
