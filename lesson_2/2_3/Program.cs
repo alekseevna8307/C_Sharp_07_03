4c0ac6a7fc0d3ac8317f230af71acf716087ad78
Anastasiya <vybornova.aa95@gmail.com>
2023-03-15 20:25:18 +0300

    Task2_3

---
+++ lesson_2/2_3/Program.cs
@@ -0,0 +1,16 @@
+﻿int num = new Random().Next(1, 1000);
+
+void PrintNumber(int num)
+{
+  Console.WriteLine(num);
+  if (num % 7 == 0 && num % 23 == 0)
+  {
+    Console.WriteLine("Число кратно одновременно 7 и 23");
+  }
+  else
+  {
+    Console.WriteLine("Число некратно одновременно 7 и 23");
+  }
+}
+
+PrintNumber(num);
