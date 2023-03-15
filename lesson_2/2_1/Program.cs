e077639e2ca1cf6f09fe1aacbc088b62070d44b5
Anastasiya <vybornova.aa95@gmail.com>
2023-03-15 19:35:40 +0300

    Task2_1

---
+++ lesson_2/2_1/Program.cs
@@ -0,0 +1,37 @@
+﻿int num = GenerateNumber();
+PrintNumber(num);
+int digit1 = GetFirstDigit(num);
+int digit2 = GetSecondDigit(num);
+int max = FindMax(digit1, digit2);
+PrintNumber(max);
+
+int GenerateNumber()
+{
+  int num = new Random().Next(10, 100);
+  return num;
+}
+
+int GetSecondDigit(int n)
+{
+  return n % 10;
+}
+
+int GetFirstDigit(int n)
+{
+  return n / 10;
+}
+
+int FindMax(int n1, int n2)
+{
+  if (n1 > n2)
+  {
+    return n1;
+  }
+  return n2;
+}
+
+void PrintNumber(int n)
+{
+  Console.WriteLine("Your number is: " + n);
+}
+
