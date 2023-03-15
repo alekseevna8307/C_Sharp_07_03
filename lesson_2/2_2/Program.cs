e688df25ddf6cddeb1b3c1eeedc8c890f565f1c6
Anastasiya <vybornova.aa95@gmail.com>
2023-03-15 19:54:29 +0300

    Task2_2

---
+++ lesson_2/2_2/Program.cs
@@ -0,0 +1,14 @@
+﻿int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+if(num1 % num2 == 0)
+{
+  Console.WriteLine("Кратно");
+}
+else
+{
+  Console.WriteLine("Не кратно, остаток: " + num1 % num2);
+}
+
+
+
