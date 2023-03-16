c7a109f28c16d29eb1ac1177e439447886200bba
Anastasiya <vybornova.aa95@gmail.com>
2023-03-17 01:46:16 +0300

    HW 1_3

---
+++ lesson_2/HW/1_3/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine()!);
+
+if (num < 100)
+{
+  Console.WriteLine("Third digit not found");
+}
+
+else
+{
+  int count = 1000;
+  while (num > count)
+  {
+    int n = num / 10;
+  }
+  Console.WriteLine("Third digit = " + num % 10);
+}
