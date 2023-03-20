f2a116177bfcd3b186b6e4ba2455403db7f7cfb0
Anastasiya <vybornova.aa95@gmail.com>
2023-03-20 23:35:01 +0300

    Task 3_1

---
+++ lesson_3/3_1/Program.cs
@@ -0,0 +1,35 @@
+﻿Diaposone();
+
+void Diaposone()
+{
+  Console.Write("Введите номер четверти - ");
+  int num = int.Parse(Console.ReadLine()!);
+
+  if (num > 4 || num < 1)
+  {
+    Console.WriteLine("введены некорректные значения");
+  }
+  else
+  {
+    switch (num)
+    {
+      case 1:
+        Console.WriteLine("x > 0, y > 0");
+        break;
+      case 2:
+        Console.WriteLine("x < 0, y > 0");
+        break;
+      case 3:
+        Console.WriteLine("x < 0, y < 0");
+        break;
+      case 4:
+        Console.WriteLine("x > 0, y < 0");
+        break;
+      default:
+        Console.WriteLine("введены некорректные значения");
+        break;
+    }
+  }
+}
+
+
