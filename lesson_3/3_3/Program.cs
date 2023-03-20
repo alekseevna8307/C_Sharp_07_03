d8a9afdc7294a630e59f610e939f9d60ed62b0d5
Anastasiya <vybornova.aa95@gmail.com>
2023-03-21 00:51:42 +0300

    Task 3_3

--- lesson_3/3_3/Program.cs
+++ lesson_3/3_3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void SquareNumber(int N)
+{
+  int i = 1;
+  while (i <= N)
+  {
+    Console.Write(Math.Pow(i, 2) + " ");
+    i++;
+  }
+}
+
+SquareNumber(N);
