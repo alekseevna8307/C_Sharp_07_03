c0e4e7f476b22d1917533f4e0db03ed8639674b9
Anastasiya <vybornova.aa95@gmail.com>
2023-03-21 23:44:32 +0300

    Task 4_0

--- lesson_4/4_0/Program.cs
+++ lesson_4/4_0/Program.cs
@@ -1,4 +1,14 @@
-﻿
+﻿int SumNumber(int num)
+{
+  int sum_all = 0;
+  for (int i = 1; i <= num; i++)
+    sum_all += i;
+  return sum_all;
+}
 
 
-Console.WriteLine("Hello, World!");
+Console.Write("Enter number -> ");
+int A = int.Parse(Console.ReadLine()!);
+
+int result = SumNumber(A);
+Console.WriteLine(result);
