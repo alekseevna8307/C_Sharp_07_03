f71e3b3101c9b7c87661f3056aca88b9cbd668b9
Anastasiya <vybornova.aa95@gmail.com>
2023-03-22 00:48:18 +0300

    Task 4_2

---
+++ lesson_4/4_2/Program.cs
@@ -0,0 +1,15 @@
+﻿int Multiply(int num)
+{
+  int mult = 1;
+  for (int i = 1; i <= num; i++)
+  {
+    mult *= i;
+  }
+  return mult;
+}
+
+Console.Write("Enter number -> ");
+int number = int.Parse(Console.ReadLine()!);
+
+int res = Multiply(number);
+Console.WriteLine(res);
