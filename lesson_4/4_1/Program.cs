a1b3f3275597d0123d9e55305bdf623fc5aaecb4
Anastasiya <vybornova.aa95@gmail.com>
2023-03-22 00:24:09 +0300

    Task 4_1

--- lesson_4/4_1/Program.cs
+++ lesson_4/4_1/Program.cs
@@ -1,4 +1,16 @@
-﻿
+﻿int CountDig(int num)
+{
+  int count = 0;
+  while (num > 0)
+  {
+    num = num / 10;
+    count += 1;
+  }
+  return count;
+}
 
+Console.Write("Enter number -> ");
+int num = int.Parse(Console.ReadLine()!);
 
-Console.WriteLine("Hello, World!");
+int res = CountDig(num);
+Console.WriteLine(res);
