3d2660a0372192cacfbdce511df43ca2bc7bb26a
Anastasiya <vybornova.aa95@gmail.com>
2023-03-21 09:50:07 +0300

    3_0 HW

---
+++ lesson_3/HW/3_0 HW/Program.cs
@@ -0,0 +1,22 @@
+﻿
+Console.Write("enter a five-digit number: ");
+int num = int.Parse(Console.ReadLine()!);
+Palindrome();
+
+void Palindrome()
+{
+  int n1 = num / 10000;
+  int n2 = num / 1000 % 10;
+  int n4 = num / 10 % 10;
+  int n5 = num % 10;
+
+  if (n1 == n5 && n2 == n4)
+  {
+    Console.WriteLine("yes");
+  }
+  else
+  {
+    Console.WriteLine("no");
+  }
+}
+
