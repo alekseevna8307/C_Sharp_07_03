c329cde99db6916ff4beaaa87914443fec956f1c
Anastasiya <vybornova.aa95@gmail.com>
2023-03-21 09:59:41 +0300

    3_2 HW

---
+++ lesson_3/HW/3_2 HW/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+
+void SquareNumber(int N)
+{
+  int i = 1;
+  while (i <= N)
+  {
+    Console.Write(Math.Pow(i, 3) + " ");
+    i++;
+  }
+}
+
+SquareNumber(N);
