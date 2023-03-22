8f963d8b7f4221f825d99603b6a1dcd0506d590a
Anastasiya <vybornova.aa95@gmail.com>
2023-03-22 22:17:02 +0300

    4_1 HW

---
+++ lesson_4/HW/4_1 HW/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number1 -> ");
+int A = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter number2 -> ");
+int B = int.Parse(Console.ReadLine()!);
+
+int Exponentiate(int A, int B)
+{
+  int prod = 1;
+  for (int i = 1; i <= B; i++)
+  {
+    prod *= A;
+  }
+  return prod;
+}
+
+int res = Exponentiate(A, B);
+Console.WriteLine(res);
