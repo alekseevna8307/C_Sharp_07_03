e8bf571adb7ab8dd6ebb22706a241598e429b804
Anastasiya <vybornova.aa95@gmail.com>
2023-03-22 17:04:39 +0300

    Task 4_3

--- lesson_4/4_3/Program.cs
+++ lesson_4/4_3/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int GenerateNumber()
+{
+  int number = new Random().Next(0, 2); // можно написать просто Next(2) - так как ноль автоматом берется.
+  return number;
+}
+
+int[] NewArray()
+{
+  int[] array = new int[8];
+
+  for (int i = 0; i < 8; i++)
+  {
+    array[i] = GenerateNumber();
+  }
+  return array;
+}
+
+void PrinArray(int[] arr)
+{
+    for (int i = 0; i < 8; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+
+PrinArray(NewArray());
+
+// другой вариант записи вывода функции:
+// int[] array = NewArray();
+// PrinArray(array);
+
