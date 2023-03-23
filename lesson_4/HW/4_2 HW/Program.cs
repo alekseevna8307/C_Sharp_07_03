6021dffeae7de31c2fdb65c6cc5e315628fde38a
Anastasiya <vybornova.aa95@gmail.com>
2023-03-23 20:07:13 +0300

    4_2 HW

---
+++ lesson_4/HW/4_2 HW/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Enter number -> ");
+int num = int.Parse(Console.ReadLine()!);
+
+int SumNumber(int num)
+{
+  int sum = num % 10;
+  int count = 0;
+  while (num > 0)
+  {
+    num = num / 10;
+    count += 1;
+    sum = sum +=num % 10;
+  }
+  return sum;
+}
+
+int res = SumNumber(num);
+Console.WriteLine(res);
