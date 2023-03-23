44a96c64ba9e01df3981a444fa919361758d181a
Anastasiya <vybornova.aa95@gmail.com>
2023-03-23 20:29:34 +0300

    4_3 HW

---
+++ lesson_4/HW/4_3 HW/Program.cs
@@ -0,0 +1,29 @@
+﻿int GenerateNumber()
+{
+  int number = new Random().Next(0, 1000);
+  return number;
+}
+
+
+int[] NewArray()
+{
+  int[] array = new int[8];
+
+  for (int i = 0; i < 8; i++)
+  {
+    array[i] = GenerateNumber();
+  }
+  return array;
+}
+
+void PrintArray(int[] array)
+{
+  for (int i = 0; i < 8; i++)
+  {
+    Console.Write($"{array[i]} ");
+  }
+  Console.WriteLine();
+}
+
+int[] array = NewArray();
+PrintArray(array);
