8500aee2a391856662a6f5782a3e0f84ec8dcf6a
Anastasiya <vybornova.aa95@gmail.com>
2023-03-26 20:34:03 +0300

    4_2 HW

--- lesson_4/HW/4_2 HW/Program.cs
+++ lesson_4/HW/4_2 HW/Program.cs
@@ -3,13 +3,12 @@ int num = int.Parse(Console.ReadLine()!);
 
 int SumNumber(int num)
 {
-  int sum = num % 10;
-  int count = 0;
+  int sum = 0;
+  
   while (num > 0)
   {
-    num = num / 10;
-    count += 1;
-    sum = sum +=num % 10;
+    sum +=num % 10;
+    num /= 10;
   }
   return sum;
 }
