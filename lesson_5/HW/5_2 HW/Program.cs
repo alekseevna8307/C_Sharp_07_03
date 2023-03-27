0a89766b99c85920e25a30a8af3d93be833ebca4
Anastasiya <vybornova.aa95@gmail.com>
2023-03-27 19:40:01 +0300

    5_2 hw

---
+++ lesson_5/HW/5_2 HW/Program.cs
@@ -0,0 +1,42 @@
+﻿void Print(int[] arr)
+{
+  int size = arr.Length;
+
+  for (int i = 0; i < size; i++)
+  {
+    Console.Write($"{arr[i]} ");
+  }
+  Console.WriteLine();
+}
+
+int[] EightMass(int size, int from, int to)
+{
+  int[] arr = new int[size];
+
+  for (int i = 0; i < size; i++)
+  {
+    arr[i] = new Random().Next(from, to + 1);
+  }
+  return arr;
+}
+
+int SumElementNechPos(int[] arr)
+{
+  int sum = 0;
+  for (int i = 0; i < arr.Length; i++)
+  {
+    if(i % 2 == 0)
+    sum += arr[i];
+  }
+  return sum;
+}
+
+int num = int.Parse(Console.ReadLine()!);
+int start = int.Parse(Console.ReadLine()!);
+int stop = int.Parse(Console.ReadLine()!);
+
+int[] mass = EightMass(num, start, stop);
+Print(mass);
+
+Console.WriteLine(SumElementNechPos(mass));
+
