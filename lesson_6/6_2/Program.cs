26bee6ebc188e65656ca7e839cb71b9d4fb697d4
Anastasiya <vybornova.aa95@gmail.com>
2023-03-28 22:56:00 +0300

    6_2

---
+++ lesson_6/6_2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Enter number -> ");
+int num = int.Parse(Console.ReadLine()!);
+
+string Change(int num)
+{
+  string new_num = string.Empty;
+
+  while (num > 0)
+  {
+    new_num = num % 2 + new_num;
+    num /= 2;
+  }
+  return new_num;
+}
+
+Console.WriteLine(Change(num));
