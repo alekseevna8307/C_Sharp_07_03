2dfa2e1336cd6635e421d769bddb7ccf4f290e59
Anastasiya <vybornova.aa95@gmail.com>
2023-03-28 23:35:46 +0300

    6_3

---
+++ lesson_6/6_3/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.Write("Enter number -> ");
+int N = int.Parse(Console.ReadLine()!);
+/*
+void Fibonacci(int N)
+{
+  int a = 0;
+  int b = 1;
+  Console.Write($"{a} {b} ");
+
+  for (int i = 2; i < N; i++)
+  {
+    Console.Write($"{a + b} ");
+    (a, b) = (b, a + b);
+  }
+}*/
+
+Fibonacci(N);
+
+void Fibonacci(int N)
+{
+  int a = 0;
+  int b = 1;
+
+  for (int i = 0; i < N; i++)
+  {
+    Console.Write($"{a} ");
+    (a, b) = (b, a + b);
+  }
+}
