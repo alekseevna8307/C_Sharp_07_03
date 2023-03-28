3ab5d27485e9090e58147183cc98dce8a56cbe41
Anastasiya <vybornova.aa95@gmail.com>
2023-03-28 17:48:25 +0300

    6_1

---
+++ lesson_6/6_1/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Write("Enter number 1 -> ");
+int n1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number 2 -> ");
+int n2 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter number 3 -> ");
+int n3 = int.Parse(Console.ReadLine()!);
+
+bool MyFunc(int n1, int n2, int n3)
+{
+  if(n1 < n2 + n3 && n2 < n1 + n3 && n3 < n1 +n2)
+  return true;
+
+  else
+  return false;
+}
+
+Console.WriteLine(MyFunc(n1, n2, n3));
