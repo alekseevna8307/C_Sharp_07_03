9211d31a5443249d5ee48d66042fa24c02a7d258
Anastasiya <vybornova.aa95@gmail.com>
2023-03-30 21:54:38 +0300

    6_2 and 6_3 hw

--- lesson_6/HW/6_2 HW/Program.cs
+++ lesson_6/HW/6_2 HW/Program.cs
@@ -1,1 +1,26 @@
-﻿
+﻿void Intersection(double b1, double k1, double b2, double k2)
+{
+  if (k1 - k2 != 0)
+  {
+  double x = (b2 - b1) / (k1 - k2);
+  double y = k1 * x + b1;
+  Console.Write($"({x};{y})");
+  }
+  
+  else if (k1 == k2 && b1 == b2) 
+  Console.Write("Прямые совпадают друг с другом");
+
+  else 
+  Console.Write("Прямые параллельные, не имеют точек пересечения");
+}
+
+Console.Write("Enter b1->");
+double b1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter k1->");
+double k1 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter b2->");
+double b2 = int.Parse(Console.ReadLine()!);
+Console.Write("Enter k2->");
+double k2 = int.Parse(Console.ReadLine()!);
+
+Intersection(b1, k1, b2, k2);
---
+++ lesson_6/HW/6_3 HW/Program.cs
@@ -0,0 +1,63 @@
+﻿void Print(int[,] arr)
+{
+  int row_size = arr.GetLength(0);
+  int col_size = arr.GetLength(1);
+
+  for (int i = 0; i < row_size; i++)
+  {
+    for (int j = 0; j < col_size; j++)
+    {
+      Console.Write($"{arr[i, j]} ");
+    }
+    Console.WriteLine();
+  }
+  Console.WriteLine();
+}
+
+int[,] MassNums(int row, int col, int from, int to)
+{
+  int[,] arr = new int[row, col];
+
+  for (int i = 0; i < row; i++)
+  {
+    for (int j = 0; j < col; j++)
+    {
+      arr[i,j] = new Random().Next(from, to + 1);
+    }
+  }
+  return arr;
+}
+
+int[,] CopyArray(int[,] arr)
+{
+  int row_size = arr.GetLength(0);
+  int col_size = arr.GetLength(1);
+  int[,] n_arr = new int[row_size, col_size];
+  
+  for (int i = 0; i < row_size; i++)
+  {
+    for (int j = 0; j < col_size; j++)
+    {
+      n_arr[i, j] = arr[i, j];
+    }
+    Console.WriteLine();
+  }
+  return n_arr;
+}
+
+
+Console.Write("Enter the number of rows: ");
+int rows = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the number of columns: ");
+int columns = int.Parse(Console.ReadLine()!);
+
+Console.Write("Enter the min number of massive: ");
+int start = int.Parse(Console.ReadLine()!);
+Console.Write("Enter the max number of massive: ");
+int stop = int.Parse(Console.ReadLine()!);
+
+int[,] mass = MassNums(rows, columns, start, stop);
+Console.WriteLine();
+Print(mass);
+int[,] n_mass = CopyArray(mass);
+Print(n_mass);
